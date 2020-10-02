cab1c8235d6d01e381b05872cb55cdad1215fb02
islam <islamhm1998@gmail.com>
2020-10-02 11:05:06 +0300

    islamChange

--- park_now-master/App.js
+++ park_now-master/App.js
@@ -10,24 +10,15 @@ import Signup from './screens/Signup';
 import SignInScreen from './screens/SignInScreen';
 import ForgetPass from './screens/ForgetPassword';
 import FindMyPosition from './screens/FindMyPosition';
+import ParkingHistory from './screens/ParkingHistory';
 const Stack = createStackNavigator();
 const isThereUser=true;
 
 const App=()=>{
 
   return(
-    <NavigationContainer >
-    {!isThereUser?(
-    <Stack.Navigator>
-    <Stack.Screen name="Welcom" component={WelcomeScreen} /> 
-    <Stack.Screen name="Sing Up" component={Signup}   navigation="Sign In"/>
-    <Stack.Screen name="Sing In" component={SignInScreen } navigation={["Sign Up","Forget Password"]}  />
-    <Stack.Screen name="Forget Password" component={ForgetPass }  /> 
-    </Stack.Navigator>)
-      :
-    <App2 name="HomeStackScreen"/>
-   }
-  </NavigationContainer>
+    <ParkingHistory/>
+  
   );
 }
 export default App;
---
+++ park_now-master/screens/ParkingHistory.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import { View, Text, FlatList, Button } from "react-native";
+import { ListItem, SearchBar ,Card} from "react-native-elements";
+
+
+ 
+class ParkingHistory extends Component {
+ 
+  constructor(props) {
+    super(props); 
+  
+    this.state = { 
+      loading: false,   
+      data: [],
+      temp: [],
+      error: null,
+      search: null
+    };
+  }
+  
+  componentDidMount() {
+    this.getData();
+  }
+ 
+   getData = async ()  => {
+    const url = `https://jsonplaceholder.typicode.com/users`;
+    this.setState({ loading: true });
+      
+     try {
+        const response = await fetch(url);
+        const json = await response.json();
+        this.setResult(json);
+     } catch (e) {
+        this.setState({ error: 'Error Loading content', loading: false });
+     }
+  };
+ 
+  setResult = (res) => {
+    this.setState({
+      data: [...this.state.data, ...res],
+      temp: [...this.state.temp, ...res],
+      error: res.error || null,
+      loading: false
+    });
+  }
+ 
+  renderHeader = () => {
+      return <SearchBar placeholder="Search Here..."
+          lightTheme round editable={true}
+          value={this.state.search}
+          onChangeText={this.updateSearch} />; 
+  }; 
+ 
+  updateSearch = search => {
+        this.setState({ search }, () => {
+            if ('' == search) {
+                this.setState({
+                    data: [...this.state.temp]
+                });
+                return;
+            }
+             
+            this.state.data = this.state.temp.filter(function(item){
+                return item.name.includes(search);
+              }).map(function({id, name, email}){
+                return {id, name, email};
+            });
+        });
+  };
+ 
+  render() {
+    return (      
+        <FlatList 
+            ListHeaderComponent={this.renderHeader}
+            data={this.state.data}
+            keyExtractor={item => item.email}
+            renderItem={({ item }) => (
+            <ListItem
+                roundAvatar
+                bottomDivider
+                title={`${item.name}`}
+                subtitle={item.email}
+            />
+          
+        )}
+      />
+      
+    );
+  }
+}
+ 
+export default ParkingHistory;
